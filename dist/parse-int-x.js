1073528134ea44a8af72fe1334bf025c0581c2dc
Graham Fairweather <xotic750@gmail.com>
2019-08-30 23:38:34 +0200

    Maintenance

--- dist/parse-int-x.js
+++ dist/parse-int-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-28T16:53:59.078Z",
+  "date": "2019-08-30T21:38:29.396Z",
   "describe": "",
   "description": "Parses a string argument and returns an integer of the specified radix.",
   "file": "parse-int-x.js",
